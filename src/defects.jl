e5c7629aeac5ce8dcfa30fbb525ceee221f3a714
navdeep rana <navdeepr@tifrh.res.in>
2022-09-18 12:23:46 +0200

    feat: Compute defects for a complex field as well.

--- src/defects.jl
+++ src/defects.jl
@@ -27,22 +27,22 @@ end
 end
 
 """
-Count all topological defects for a 2D vector field.
+Count all topological zeros for a 2D orientation field.
 
 $(SIGNATURES)
 """
-function count_topological_charges(theta::Array{Float64,2}; threshold = 0.8e0 * (2π))
-    nx, ny = size(theta)
+function count_topological_charges(θ::Array{T,2}; threshold = 0.8 * (2π)) where {T<:Real}
+    nx, ny = size(θ)
     x, y, c = Int[], Int[], Int[]
 
     @inbounds for j = 1:ny
         jnext = _get_next(j, ny)
         @inbounds for i = 1:nx
             inext = _get_next(i, nx)
-            t1 = theta[i, j]
-            t2 = theta[inext, j]
-            t3 = theta[inext, jnext]
-            t4 = theta[i, jnext]
+            t1 = θ[i, j]
+            t2 = θ[inext, j]
+            t3 = θ[inext, jnext]
+            t4 = θ[i, jnext]
 
             charge = _area(t2, t1) + _area(t3, t2) + _area(t4, t3) + _area(t1, t4)
 
@@ -56,8 +56,12 @@ function count_topological_charges(theta::Array{Float64,2}; threshold = 0.8e0 * 
     return x, y, c
 end
 
-function count_topological_charges(u::T, v::T; kwargs...) where {T<:Array{Float64,2}}
-    theta = @. atan(v, u)
-    return count_topological_charges(theta; kwargs...)
+function count_topological_charges(u::Array{T,2}, v::Array{T,2}; kwargs...) where {T<:Real}
+    θ = @. atan(v, u)
+    return count_topological_charges(θ; kwargs...)
 end
 
+function count_topological_charges(ϕ::Array{Complex{T},2}; kwargs...) where {T<:Real}
+    θ = @. angle(ϕ)
+    return count_topological_charges(θ; kwargs...)
+end
