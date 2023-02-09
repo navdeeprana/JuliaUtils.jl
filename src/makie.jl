8fad75c1007e7ed49e7f6a13d3e24eaad30e0e9f
navdeep rana <navdeepr@tifrh.res.in>
2023-02-09 12:14:35 +0100

    fix: Better list comprehension

--- src/makie.jl
+++ src/makie.jl
@@ -3,7 +3,7 @@ using Makie: Figure, Axis, AxisAspect
 function figax(; nx = 1, ny = 1, h = 3, a = 1.6, s = 100, kwargs...)
     res = (round(Int, a * s * h * nx), round(Int, s * h * ny))
     fig = Figure(resolution = res)
-    ax = [Axis(fig[j, i]; aspect = AxisAspect(a), kwargs...) for i in 1:nx for j in 1:ny]
+    ax = [Axis(fig[j, i]; aspect = AxisAspect(a), kwargs...) for i in 1:nx, j in 1:ny]
     if nx * ny == 1
         return fig, ax[1]
     else
