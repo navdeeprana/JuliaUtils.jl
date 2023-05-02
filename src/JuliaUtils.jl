1415b979962c427141bd03f607bd2c07b79650d5
pankajpopli <43290036+pankajpopli@users.noreply.github.com>
2023-05-02 21:05:54 +0530

    Update JuliaUtils.jl

--- src/JuliaUtils.jl
+++ src/JuliaUtils.jl
@@ -1,54 +1,61 @@
 """
-Main module for `JuliaUtils.jl` -- my julia language utility functions.
-
+Main module for `Julia Useful functions`
 # Exports
 $(EXPORTS)
 """
 module JuliaUtils
+   export read_data,
+      read_xydata,
+      write_data,
+      write_xydata,
+      linspace,
+      logspaced,
+      cartesian_mesh,
+      polar_mesh,
+      periodic_distance,
+      aperiodic,
+      defect_vector_field,
+      find_defects,
+      charge,
+      nearest_neighbour_distance,
+      radial_distribution_function,
+      Interp2D
+   
+   using DocStringExtensions
+   include("io.jl")
+   include("space.jl")
+   include("defects.jl")
+   include("physics.jl")
+   include("macros.jl")
+   include("interpolations.jl")
+   include("periodic.jl")
+   
+   # include all files recursively from the `src` folder
+   # for (root, dirs, files) in walkdir("src")
+   #    for file in files
+   #       # check if the file is a Julia file
+   #       if endswith(file, ".jl")
+   #          # construct the path to the file
+   #          path = joinpath(root, file)
+   #          # include the file in the module
+   #          Base.include(path)
+   #       end
+   #    end
+   # end
 
-export read_data,
-    read_xydata,
-    write_data,
-    write_xydata,
-    linspace,
-    logspaced,
-    cartesian_mesh,
-    polar_mesh,
-    periodic_distance,
-    aperiodic,
-    defect_vector_field,
-    find_defects,
-    charge,
-    nearest_neighbour_distance,
-    radial_distribution_function,
-    @repeat
+   module PyPlotUtils
+      export colors,
+         figax,
+         noticks!,
+         fontsize!,
+         formatter!
+         include("pyplot.jl")
+   end
 
-using DocStringExtensions
+   module MakieUtils
+   export figax
+      include("makie.jl")
+   end
 
-include("io.jl")
-include("space.jl")
-include("defects.jl")
-include("physics.jl")
-include("macros.jl")
-
-module PyPlotUtils
-
-export colors,
-    figax,
-    noticks!,
-    fontsize!,
-    formatter!
-
-include("pyplot.jl")
-
-end
-
-module MakieUtils
-
-export figax
-
-include("makie.jl")
-
-end
 
 end
